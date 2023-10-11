8e0d06799c8bc6b4c22bed3a0b30e8827989e585
5012Farah <fh5012@gmail.com>
2023-10-11 22:29:29 +0200

    git commit

--- 01_PHP_AUTOLOAD/DB/ConnectDB.php
+++ 01_PHP_AUTOLOAD/DB/ConnectDB.php
@@ -3,5 +3,18 @@
 // Database connection
 Class ConnectDB 
 {
-    
+    private $host;
+    private $dbname;
+    private $username;
+    private $password;
+    private $chartset;
+
+    public function connect()
+    {
+        $this->host = "localhost";
+        $this->dbname ="blogi";
+        $this->username ="root";
+        $this->password ="";
+        $this->chartset ="utf_8";
+    }
 }
